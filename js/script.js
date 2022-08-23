85bcb687606c4833149bb799a6f15d0644a55661
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-08-23 14:49:01 +0600

    player expenses added

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ function diplayPlayer(player){
 
     const listBody = document.getElementById('list-body');
     listBody.innerHTML ="";
+    const playerLength = player.length;
 
-    for(let i = 0; i < player.length ; i++){
+    for(let i = 0; i < playerLength ; i++){
         
         const name = playerArray[i].Name;
         const li =document.createElement('li');
@@ -31,3 +32,34 @@ function selected(element){
     playerArray.push(playerObj);
     diplayPlayer(playerArray);   
 }
+function getAmountValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    if(isNaN(inputFieldValue)){
+        alert('Enter a Valid Number');
+        return inputFieldValue;
+    }
+    return inputFieldValue;
+}
+
+document.getElementById('player-calculate').addEventListener('click', function(){
+    const playerAmount = getAmountValueById('player-amount');
+    const listAll = document.querySelectorAll('#list-body li') ;
+    const PlayerExpenses = document.getElementById('total-player-Cost');
+    const totalPlayerExpensesString = PlayerExpenses.innerText;
+    const totalPlayerExpenses = parseFloat(totalPlayerExpensesString);
+    
+    const playerCost = playerAmount * listAll.length;
+
+    const totalPlayerCost = totalPlayerExpenses + playerCost;
+    PlayerExpenses.innerText = totalPlayerCost;
+
+
+
+    
+    
+    
+    
+})
