24a59fec748bb82e75ce938e0e57cc7308ffeded
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-08-23 12:39:38 +0600

    selected five player

--- js/script.js
+++ js/script.js
@@ -1,43 +1,33 @@
 const playerArray = [];
 function diplayPlayer(player){
 
+    if(player.length > 5){
+        alert('You can select only 5 players');
+        return ;   
+    }
+
     const listBody = document.getElementById('list-body');
     listBody.innerHTML ="";
 
     for(let i = 0; i < player.length ; i++){
         
         const name = playerArray[i].Name;
-
         const li =document.createElement('li');
-
+        
         li.innerHTML = `
         ${name}
         `
-        
-        
-        listBody.appendChild(li)
+        listBody.appendChild(li)  
     }
-    if(player.length > 5){
-        alert('You can select only 5 players');
-    }
-    
-
-    
-    
-    
-
 }
 function selected(element){
 
     const playerName = element.parentNode.children[0].innerText;
-
 
     const playerObj ={
         Name : playerName,
     }
         
     playerArray.push(playerObj);
-    diplayPlayer(playerArray);
-
-    
+    diplayPlayer(playerArray);   
 }
